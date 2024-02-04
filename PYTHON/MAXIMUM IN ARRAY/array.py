85a5851a7796010af7e44557d04a0295f8466cd3
neuqs0035 <bhavyapadaliya179@gmail.com>
2024-02-04 08:12:45 +0530

    COMMENTS ADDED FOR METHOD 1

--- PYTHON/MAXIMUM IN ARRAY/array.py
+++ PYTHON/MAXIMUM IN ARRAY/array.py
@@ -1,5 +1,9 @@
 arr = [1,45,78,345,678,3465,87,45]
 
+# Finding Maximum Element From Array 
+
+
+# Method 1 : By Iterating Over Whole Array And Check Every Element
 def method1(arr):
     max = arr[0]
 
