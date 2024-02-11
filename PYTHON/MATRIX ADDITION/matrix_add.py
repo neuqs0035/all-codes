2a93d6b80dfedfa5be7dd8283a63265967a741ce
neuqs0035 <bhavyapadaliya179@gmail.com>
2024-02-11 18:08:13 +0530

    INPUT CODE ADDED TO TAKE SIZE OF MATRICES AND DISPLAY OUTPUT MATRIX

--- PYTHON/MATRIX ADDITION/matrix_add.py
+++ PYTHON/MATRIX ADDITION/matrix_add.py
@@ -49,3 +49,26 @@ def display_matrix(matrix):
 		print("|")
 
 	print(f"\nOrder = {len(matrix)} x {len(matrix[0])}\n")
+	
+
+print("\nFor Matrix 1 :-")
+m1_row_count = pyin.inputInt(prompt="\nEnter No Of Rows : ",greaterThan=0)
+m1_col_count = pyin.inputInt(prompt="\nEnter No Of Columns : ",greaterThan=0)
+
+print("\nFor Matrix 2 :-")
+m2_row_count = pyin.inputInt(prompt="\nEnter No Of Rows : ",greaterThan=0)
+m2_col_count = pyin.inputInt(prompt="\nEnter No Of Columns : ",greaterThan=0)
+
+if (m1_col_count+m1_row_count) != (m2_col_count+m2_row_count):
+	print("\nBoth Matrix's Orders Are Not Same , Addition Is Not Possible")
+	
+else:
+	
+	print("\nFor Matrix 1 :-")
+	matrix1 = input_matrix(m1_row_count,m1_col_count)
+	print("\nFor Matrix 2 :-")
+	matrix2 = input_matrix(m1_row_count,m1_col_count)
+
+	output_matrix = add_matrix(matrix1,matrix2)
+	print("\nOutput Matrix\n")
+	display_matrix(output_matrix)
